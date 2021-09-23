fb37f59182ce017028b165f2880313c54c10795b
Michael Dorado <52438439+mdoradocode@users.noreply.github.com>
2021-09-23 11:11:56 -0700

    Commenting and documentation

--- PA1/mdorado_pa1.py
+++ PA1/mdorado_pa1.py
@@ -2,36 +2,39 @@
 # Date: 9/6/2021
 # Class: CS 457 Database Managment Systems 
 # Project: This project is intended to be a bare bones version of a DBMS that can create and delete databases(and their associated tables), create and delete tables (that will be in a subdirectory under the database), and query and update the tables. I would like to implement a GUI to work along side this project, but I am not sure how feasible that is with the time constraints.
-# Functions to implement
-#   Help function to display commands
-#   CREATE TABLE tbl_1 (a1 int, a2 varchar(20))
-#   CREATE TABLE tbl_2 (a1 Float, a2 varchar(40))
-#   CREATE TABLE tbl_3 (a1 longInt, a2 varchar(20), a3 string(60))
-#   SELECT * FROM 
 
 ##---Libraries---##
-#This allows me to interact with CSV files easily
+#This allows me to interact with CSV files easily, there is no "managment" of files or directories, it is only used to read and write from industry standard csv files
 import csv
 #This allows for easy file work and listing
 import os
-from os import path, read, walk
+from os import path, walk
 #This allows for the path of the current directory and script directory to be easily found
 import pathlib
 import sys
+#This is needed to delete directories
 import shutil
 
 ##---Global Variables---##
-#For directories and taking in commands
+#Take in user input commands
 commandWhole = None
+#Take in user attributes
 argumentsWhole = None
+#Split up the current command into a list
 commandSplit = None
+#Split up the current attribute input into a list
 argumentsSplit = None
-##scriptDir = pathlib.Path(__file__).parent.resolve()
+#This begins as the directory the file of the project resides in and follows the working directory
 currentDir = pathlib.Path(__file__).parent.resolve()
+#Used to set the current subdirectory without changing the directory
 path = None
+#List of all currently present databases
 databaseList = []
+#The name of the current database for use in file path definition
 currentDB = []
+#Menu while loop controller
 menuControl = 1
+#Used to assist in the query function
 rows = []
 
 
@@ -43,11 +46,12 @@ def main():
         commandInterpt()
     print("All Done.")
 
+
+#Parse out the user input into cascading if else statments to decide on program direction
 def commandInterpt():
     global commandSplit, currentDir,currentDB,menuControl
     #All commands that begin with CREATE, which can be followed by TABLE or DATABASE
     if commandSplit[0] == 'CREATE':
-        ##This also needs to make sure you are accessing a database
         if commandSplit[1] == 'TABLE':
             if currentDB != []:
                 createTable()
@@ -55,6 +59,7 @@ def commandInterpt():
                 print("!Failed to make " + commandSplit[2] + " because you are not in a Database")
         elif commandSplit[1] == 'DATABASE':
                 createDatabase(commandSplit[2])
+
     elif commandSplit[0] == 'USE':
         ##A Database will be the only command to follow this one
         if commandSplit[1] in databaseList:
@@ -63,11 +68,13 @@ def commandInterpt():
         else:
             ##Send them back to the command loop
             print("!Failed to use " + commandSplit[1])
+
     elif commandSplit[0] == 'DROP':
         if commandSplit[1] == 'TABLE':
             dropTable()
         elif commandSplit[1] == 'DATABASE':
             dropDatabase()
+
     elif commandSplit[0] == 'SELECT':
         #Will always be followed by * FROM tableName
         #Will need to be expanded for future projects
@@ -75,6 +82,7 @@ def commandInterpt():
             fullFileRead()
         else:
             print('!Failed not currently in a database.')
+
     elif commandSplit[0] == 'ALTER':
         if commandSplit[1] == 'TABLE':
             if commandSplit[3] == 'ADD':
@@ -84,28 +92,43 @@ def commandInterpt():
         menuControl = 0
     else:
         print("!Failed: Command not recognized")
+
+
+#This allows an attribute of a table to be added after the creation of a table
 def alterTable():
     global commandSplit
+    #This gives a special os variable that allows me to access the correct directory
     fullName = os.path.join(getCurrentDir(),commandSplit[2]+'.csv')
     tempList = []
     existingRow = []
+    #This parses up the attributes to be added and appends it into a neat list
     for command in range(4, len(commandSplit),2):
         tempCommand = commandSplit[command] + ' ' + commandSplit[command+1]
         tempList.append(tempCommand)
+    #This opens the table that is being altered and reads the first row (attributes) of the table and creates a list of the original attributes
     with open(fullName,'r',encoding='UTF8') as s:
         reader = csv.reader(s)
         existingRow = list(reader)
+    #Combines the new list and the existing list
     finalRow = existingRow[0] + tempList
+    #Writes the new attribute list to the file (this recreates the file, so this implementation will need to be adjusted for the next project)
     with open(fullName, 'w', encoding='UTF8',newline='') as f:
         writer = csv.writer(f)
         writer.writerow(finalRow)
     print("Updated Table " + commandSplit[2])
+
+
+#Make the rows variable an empty list so that it may be used again to read the tables
 def clearRows():
     global rows
     rows = []
+
+
+#Read the whole file top to bottom and display its contents 
 def fullFileRead():
     global commandSplit
     fullName = os.path.join(getCurrentDir(), commandSplit[3] + '.csv')
+    #Reads the whole file and throws an error if the action could not be completed
     try:
         with open(fullName,"r",encoding="UTF8") as source:
             reader = csv.reader(source)
@@ -118,6 +141,8 @@ def fullFileRead():
     except:
         print("!Failed to read file.")
 
+
+#Delete a table
 def dropTable():
     global commandSplit
     fullName = os.path.join(getCurrentDir(), commandSplit[2] + '.csv')
@@ -127,6 +152,8 @@ def dropTable():
     except OSError:
         print("!Failed could not drop table " + commandSplit[2] + ", may not be in current database.")
 
+
+#Delete a database
 def dropDatabase():
     global commandSplit
     try: 
@@ -135,19 +162,25 @@ def dropDatabase():
     except OSError:
         print('!Failed could not drop database ' + commandSplit[2])
 
+
+#Get the current directory and modify the actual variable to be working in a descrete database
 def getCurrentDir():
     global currentDB, currentDir
     return os.path.join(currentDir, currentDB)
 
 
+#Create a table
 def createTable():
     global commandSplit
     tempDir = getCurrentDir()
     fullName = os.path.join(tempDir,commandSplit[2]+'.csv')
+    #This checks to make sure that the database doesnt already exist before trying to create the database
     if os.path.exists(fullName) == False:
         try:
+            #If there is no arguements or a database with no attributes
             if argumentsSplit == ['']:
                 f = open(fullName, "x")
+            #This is the clause for table creation with arguments
             else:
                 with open(fullName, 'w', encoding='UTF8',newline='') as f:
                     writer = csv.writer(f)
@@ -158,6 +191,8 @@ def createTable():
     else:
         print("!Failed to create table " + commandSplit[2] + " because it already exists")
 
+
+#Create a database
 def createDatabase(databaseName):
     ##global path
     path = os.path.join(currentDir,databaseName)
@@ -167,24 +202,31 @@ def createDatabase(databaseName):
     except OSError as error:
         print('!Failed to create database "' + databaseName + '" because it already exists.')
 
-##This will gather a list of current databases##
+
+
+##This will gather a list of current databases
 def compileDatabaseList():
-    global scriptDir, databaseList
+    global databaseList
     databaseList = []
+    #Walk the current directory and gather the names of all the subdirectories 
     for (dirPath, dirNames, fileNames) in walk(currentDir):
         databaseList.extend(dirNames)
 
-##This takes in command line user prompts and turns them into lists for access##
+
+
+##This takes in command line user prompts and turns them into lists for access
 def takeCommand():
     global commandWhole,argumentsWhole,argumentsSplit,commandSplit
     tempInput = input('Enter Command: ')
     try:
+        #This is the clause for commands with attribute arguments to parse up
         commandWhole, argumentsWhole = tempInput.split(' (')
         ##This is needed to take off the trailing ');'
         argumentsWhole = argumentsWhole.removesuffix(');')
         argumentsSplit = argumentsWhole.split(', ')
         commandSplit = commandWhole.split(' ')
     except ValueError:
+        #This is the clause for commmands that do not involve attributes/arguements
         commandWhole = tempInput.removesuffix(';')
         commandSplit = commandWhole.split(' ')
 
